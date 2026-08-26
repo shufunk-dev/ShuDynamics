050414c3dfe63d688442e907623092c2e2ce9dc4
shufunk-dev <shufunk@gmail.com>
2026-08-26 07:16:23 -0400

    Update RoadTransitionRamp with solid base geometry and clean blockstate

--- src/main/java/net/enchantedwood/block/custom/RoadTransitionRampBlock.java
+++ src/main/java/net/enchantedwood/block/custom/RoadTransitionRampBlock.java
@@ -5,7 +5,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.block.enums.BlockHalf;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
@@ -21,65 +20,36 @@ import net.minecraft.world.BlockView;
 public class RoadTransitionRampBlock extends HorizontalFacingBlock {
     public static final MapCodec<RoadTransitionRampBlock> CODEC = createCodec(RoadTransitionRampBlock::new);
     public static final EnumProperty<Direction> FACING = Properties.HORIZONTAL_FACING;
-    public static final EnumProperty<BlockHalf> HALF = Properties.BLOCK_HALF;
 
-    // Bottom Half Shapes (0 to 8px)
-    private static final VoxelShape SHAPE_BOTTOM_NORTH = VoxelShapes.union(
-            Block.createCuboidShape(0, 0, 0, 16, 8, 4),
-            Block.createCuboidShape(0, 0, 4, 16, 6, 8),
+    // 4-Tier Solid Ramp (0 to 8px height)
+    private static final VoxelShape SHAPE_NORTH = VoxelShapes.union(
+            Block.createCuboidShape(0, 0, 12, 16, 2, 16),
             Block.createCuboidShape(0, 0, 8, 16, 4, 12),
-            Block.createCuboidShape(0, 0, 12, 16, 2, 16)
+            Block.createCuboidShape(0, 0, 4, 16, 6, 8),
+            Block.createCuboidShape(0, 0, 0, 16, 8, 4)
     );
-    private static final VoxelShape SHAPE_BOTTOM_SOUTH = VoxelShapes.union(
-            Block.createCuboidShape(0, 0, 12, 16, 8, 16),
-            Block.createCuboidShape(0, 0, 8, 16, 6, 12),
+    private static final VoxelShape SHAPE_SOUTH = VoxelShapes.union(
+            Block.createCuboidShape(0, 0, 0, 16, 2, 4),
             Block.createCuboidShape(0, 0, 4, 16, 4, 8),
-            Block.createCuboidShape(0, 0, 0, 16, 2, 4)
+            Block.createCuboidShape(0, 0, 8, 16, 6, 12),
+            Block.createCuboidShape(0, 0, 12, 16, 8, 16)
     );
-    private static final VoxelShape SHAPE_BOTTOM_WEST = VoxelShapes.union(
-            Block.createCuboidShape(0, 0, 0, 4, 8, 16),
-            Block.createCuboidShape(4, 0, 0, 8, 6, 16),
+    private static final VoxelShape SHAPE_WEST = VoxelShapes.union(
+            Block.createCuboidShape(12, 0, 0, 16, 2, 16),
             Block.createCuboidShape(8, 0, 0, 12, 4, 16),
-            Block.createCuboidShape(12, 0, 0, 16, 2, 16)
+            Block.createCuboidShape(4, 0, 0, 8, 6, 16),
+            Block.createCuboidShape(0, 0, 0, 4, 8, 16)
     );
-    private static final VoxelShape SHAPE_BOTTOM_EAST = VoxelShapes.union(
-            Block.createCuboidShape(12, 0, 0, 16, 8, 16),
-            Block.createCuboidShape(8, 0, 0, 12, 6, 16),
+    private static final VoxelShape SHAPE_EAST = VoxelShapes.union(
+            Block.createCuboidShape(0, 0, 0, 4, 2, 16),
             Block.createCuboidShape(4, 0, 0, 8, 4, 16),
-            Block.createCuboidShape(0, 0, 0, 4, 2, 16)
-    );
-
-    // Top Half Shapes (8px to 16px)
-    private static final VoxelShape SHAPE_TOP_NORTH = VoxelShapes.union(
-            Block.createCuboidShape(0, 8, 0, 16, 16, 4),
-            Block.createCuboidShape(0, 8, 4, 16, 14, 8),
-            Block.createCuboidShape(0, 8, 8, 16, 12, 12),
-            Block.createCuboidShape(0, 8, 12, 16, 10, 16)
-    );
-    private static final VoxelShape SHAPE_TOP_SOUTH = VoxelShapes.union(
-            Block.createCuboidShape(0, 8, 12, 16, 16, 16),
-            Block.createCuboidShape(0, 8, 8, 16, 14, 12),
-            Block.createCuboidShape(0, 8, 4, 16, 12, 8),
-            Block.createCuboidShape(0, 8, 0, 16, 10, 4)
-    );
-    private static final VoxelShape SHAPE_TOP_WEST = VoxelShapes.union(
-            Block.createCuboidShape(0, 8, 0, 4, 16, 16),
-            Block.createCuboidShape(4, 8, 0, 8, 14, 16),
-            Block.createCuboidShape(8, 8, 0, 12, 12, 16),
-            Block.createCuboidShape(12, 8, 0, 16, 10, 16)
-    );
-    private static final VoxelShape SHAPE_TOP_EAST = VoxelShapes.union(
-            Block.createCuboidShape(12, 8, 0, 16, 16, 16),
-            Block.createCuboidShape(8, 8, 0, 12, 14, 16),
-            Block.createCuboidShape(4, 8, 0, 8, 12, 16),
-            Block.createCuboidShape(0, 8, 0, 4, 10, 16)
+            Block.createCuboidShape(8, 0, 0, 12, 6, 16),
+            Block.createCuboidShape(12, 0, 0, 16, 8, 16)
     );
 
     public RoadTransitionRampBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.stateManager.getDefaultState()
-                .with(FACING, Direction.NORTH)
-                .with(HALF, BlockHalf.BOTTOM));
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
     }
 
     @Override
@@ -89,44 +59,22 @@ public class RoadTransitionRampBlock extends HorizontalFacingBlock {
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        Direction dir = state.get(FACING);
-        boolean isTop = state.get(HALF) == BlockHalf.TOP;
-        if (isTop) {
-            return switch (dir) {
-                case SOUTH -> SHAPE_TOP_SOUTH;
-                case WEST -> SHAPE_TOP_WEST;
-                case EAST -> SHAPE_TOP_EAST;
-                default -> SHAPE_TOP_NORTH;
-            };
-        } else {
-            return switch (dir) {
-                case SOUTH -> SHAPE_BOTTOM_SOUTH;
-                case WEST -> SHAPE_BOTTOM_WEST;
-                case EAST -> SHAPE_BOTTOM_EAST;
-                default -> SHAPE_BOTTOM_NORTH;
-            };
-        }
+        return switch (state.get(FACING)) {
+            case SOUTH -> SHAPE_SOUTH;
+            case WEST -> SHAPE_WEST;
+            case EAST -> SHAPE_EAST;
+            default -> SHAPE_NORTH;
+        };
     }
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        BlockPos pos = ctx.getBlockPos();
-        Direction side = ctx.getSide();
-        Direction playerFacing = ctx.getHorizontalPlayerFacing().getOpposite();
-
-        BlockHalf half = BlockHalf.BOTTOM;
-        if (side == Direction.DOWN || (side != Direction.UP && ctx.getHitPos().y - (double)pos.getY() > 0.5)) {
-            half = BlockHalf.TOP;
-        }
-
-        return this.getDefaultState()
-                .with(FACING, playerFacing)
-                .with(HALF, half);
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING, HALF);
+        builder.add(FACING);
     }
 
     @Override
